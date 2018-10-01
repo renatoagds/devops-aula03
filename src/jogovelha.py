c56f106715578fccb2cbdc9829c15cd8b3d5ad7b
renatoagds <renato_0603@hotmail.com>
2018-10-01 20:34:21 -0300

    add error

--- src/jogovelha.py
+++ src/jogovelha.py
@@ -3,7 +3,7 @@ def inicializar():
     for i in range(3):
         linha = []
         for j in range(3):
-            linha.append(".")
+            linha.append("X")
         tab.append(linha)
     return tab
 
